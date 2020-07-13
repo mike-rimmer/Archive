c36f3321d3585eaee57c34f4f4a45aa366387934
Mike Rimmer <mrimmer01@gmail.com>
2020-07-13 11:27:26 -0400

    Queryfrm implemented

---
+++ src/components/QueryFrm.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="queryform">
+    <div>
+      <h1>{{formTitle}}</h1>
+      <p>
+        {{aboutRegistry}}
+      </p>
+
+    </div>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="generalSearch"
+        :counter="10"
+        label="Search all Fields by keyword(s) and boolean operators"
+        required
+      ></v-text-field>
+
+      <v-row class="justify-space-around">
+        <v-col cols="12" sm="6" md="2">
+          <v-select
+          width = 200;
+          v-model = 'shipname'
+          :items='ships'
+          label='Select Vessel Name'
+          required
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="6" md="2">
+          <v-select
+          :items='prov'
+          label='Select Province '
+          required
+          ></v-select>
+        </v-col>
+
+        <v-col cols="12" sm="6" md="2">
+         <v-select
+          v-model ='yearselect'
+          :items='years'
+          label='Select Year'
+          required
+          ></v-select>
+        </v-col>
+        <v-col cols="12" sm="6" md="2">
+          <v-text-field
+          width=200
+          v-model="officialNumbers"
+          label="Select Official Numbers"
+          required>
+          </v-text-field>
+        </v-col>
+      </v-row>
+
+<v-row >
+  <v-col>
+      <v-text-field
+        v-model="reasonClosedRemarkNotes"
+        label="Reason Closed & Remarks & Notes"
+        required
+      ></v-text-field>
+  </v-col>
+
+  </v-row>
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="getRecord">Validate</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      officialNumbers: '',
+      reasonClosedRemarkNotes: '',
+      generalSearch: '',
+      noTitle: true,
+      picker: '',
+      aboutRegistry: 'This is a test',
+      shipname: '',
+      yearselected: '',
+      valid: false,
+      prov: [
+        'Alberta',
+        'British Columbia',
+        'Manitoba',
+        'New Brunswick',
+        'Newfoundland',
+        'Labrador',
+        'Nova Scotia',
+        'Ontario',
+        'Prince Edward Island',
+        'Quebec',
+        'Saskatchewan',
+      ],
+      // year :[1800]
+    };
+  },
+  props: {
+    ships: Array,
+    formTitle: String,
+    years: Array,
+  },
+
+  methods: {
+    getRecord() {
+      alert('Record Requested');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.dqueryform {
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,102 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='home'>
+    <v-card max-width='375'
+    class='cols 12 col-sm-3"
+    lightblue my-16'>
+      <v-select
+        v-model="activelist"
+        :items = 'shiplistTitle'
+        label = 'Please pick your Ship Registry'
+      ></v-select>
+    </v-card>
+    <QueryFrm
+      v-bind:ships = "shipnames"
+      v-bind:years = "millsyears"
+      v-bind:formTitle = 'activelist'
+    ></QueryFrm>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import QueryFrmMills from '@/components/QueryFrmMills.vue';
+
+import QueryFrm from '@/components/QueryFrm.vue';
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld,
+    QueryFrm,
+
+  },
+
+  data() {
+    return {
+      activelist: 'Please Select Ship Registry',
+      shiplistTitle: [
+        'Canadian Registry List',
+        'Canada Steamship Lines Fleet List',
+        'Mills List',
+        'Wallace List',
+        'Ship Owners List',
+      ],
+
+      shiplistDesc: [
+        'The Canadian Registry List',
+        'Canada Steamship Lines - Old Ship Register',
+        "The New Mills' List Registered Canadian Steamships 1817 - 1930 over 75 feet",
+        'Wallace Ship List',
+        'Ship Owners List',
+      ],
+      shipnames: [],
+      millsyears: [],
+    };
+  },
+  methods: {
+    // getDataBase(db) {
+    //   alert(`${db} Selected`);
+    // },
+    getMillsShipName() {
+      fetch('http://localhost/phpRestArchive/serve/api/Mills/readshipnames.php')
+        .then(
+          (response) => response.json(),
+        )
+        .then(
+          (data) => {
+            this.shipnames = data;
+            console.log('Mikey was here');
+          },
+        );
+    },
+  },
+  created() {
+    fetch('http://localhost/phpRestArchive/serve/api/Mills/readshipnames.php')
+      .then(
+        (response) => response.json(),
+      )
+      .then(
+        (data) => {
+          this.shipnames = data;
+          console.log('Mikey was here');
+        },
+      );
+    fetch('http://localhost/phpRestArchive/serve/api/Mills/getdates.php')
+      .then(
+        (response) => response.json(),
+      )
+      .then(
+        (data) => {
+          this.millsyears = data;
+          console.log('Mikey was here');
+        },
+      );
   },
 };
 </script>
+<style scoped>
+.home{
+  width:80%;
+  margin:0 auto;
+}
+li:hover {
+}
+</style>
